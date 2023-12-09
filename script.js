d9cc7b0fd992a7f7c0bef3b0fd037043b884bba0
amwedd <149416692+amwedd@users.noreply.github.com>
2023-12-09 17:36:16 +0700

    delete comment

--- script.js
+++ script.js
@@ -13,38 +13,38 @@ simplyCountdown(".simply-countdown", {
   },
 });
 
-//   WISHES
-const scriptURL = "https://script.google.com/macros/s/AKfycbzo9gt5FxH48wWXn6XSP2Ycm_GodjmTZF9vpWvg1XQvmMvlW4pzfrOS1eCznDzaHJa_/exec";
-const form = document.forms["wishes"];
-const btnKirim = document.querySelector(".btn-kirim");
-const btnLoading = document.querySelector(".btn-load");
-const myAlert = document.querySelector(".my-alert");
-const btnClose = document.querySelector(".btn-close");
+// //   WISHES
+// const scriptURL = "https://script.google.com/macros/s/AKfycbzo9gt5FxH48wWXn6XSP2Ycm_GodjmTZF9vpWvg1XQvmMvlW4pzfrOS1eCznDzaHJa_/exec";
+// const form = document.forms["wishes"];
+// const btnKirim = document.querySelector(".btn-kirim");
+// const btnLoading = document.querySelector(".btn-load");
+// const myAlert = document.querySelector(".my-alert");
+// const btnClose = document.querySelector(".btn-close");
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  // ketika tombol submit diklik
-  // tampilkan tombol lodaing, hilangkan tombol kirim
-  btnKirim.classList.toggle("d-none");
-  btnLoading.classList.toggle("d-none");
+// form.addEventListener("submit", (e) => {
+//   e.preventDefault();
+//   // ketika tombol submit diklik
+//   // tampilkan tombol lodaing, hilangkan tombol kirim
+//   btnKirim.classList.toggle("d-none");
+//   btnLoading.classList.toggle("d-none");
 
-  fetch(scriptURL, { method: "POST", body: new FormData(form) })
-    .then((response) => {
-      // tampilkan tombol kirim, hilangkan tombol loading
-      btnKirim.classList.toggle("d-none");
-      btnLoading.classList.toggle("d-none");
-      // tampilkan alert
-      myAlert.classList.toggle("d-flex");
-      // reset form
-      form.reset();
-      console.log("Success!", response);
-    })
-    .catch((error) => console.error("Error!", error.message));
-});
+//   fetch(scriptURL, { method: "POST", body: new FormData(form) })
+//     .then((response) => {
+//       // tampilkan tombol kirim, hilangkan tombol loading
+//       btnKirim.classList.toggle("d-none");
+//       btnLoading.classList.toggle("d-none");
+//       // tampilkan alert
+//       myAlert.classList.toggle("d-flex");
+//       // reset form
+//       form.reset();
+//       console.log("Success!", response);
+//     })
+//     .catch((error) => console.error("Error!", error.message));
+// });
 
-btnClose.addEventListener("click", function () {
-  myAlert.classList.toggle("d-flex");
-});
+// btnClose.addEventListener("click", function () {
+//   myAlert.classList.toggle("d-flex");
+// });
 
 // AUDIO
 function showInvitation() {
